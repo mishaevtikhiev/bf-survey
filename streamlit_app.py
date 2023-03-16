fb8e4d879e20aebbfb1c329f5a520b0b0de28ca4
Misha Evtikhiev <mikhail.evtikhiev@jetbrains.com>
2023-03-16 10:11:33 +0200

    first toy streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,24 @@
+import streamlit as st
+
+# Define the questions and possible answers
+questions = {
+    "What is your favorite color?": ["Red", "Green", "Blue"],
+    "What is your favorite animal?": ["Dog", "Cat", "Fish"],
+    "What is your favorite food?": ["Pizza", "Burgers", "Tacos"]
+}
+
+# Initialize a dictionary to store the user's answers
+answers = {}
+
+# Loop through the questions and present them to the user
+for question, choices in questions.items():
+    # Use Streamlit's `radio` widget to create a multiple-choice question
+    answer = st.radio(question, choices)
+    # Store the user's answer in the dictionary
+    answers[question] = answer
+
+# Display the user's answers
+st.write("Here are your answers:")
+for question, answer in answers.items():
+    st.write(f"- {question}: {answer}")
 
